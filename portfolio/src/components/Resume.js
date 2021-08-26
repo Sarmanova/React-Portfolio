d486fc39dbcb0bdc9a07880649a1b7239ff5e7e4
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2021-08-25 21:12:39 -0500

    added my resume

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -2,11 +2,11 @@ import React from 'react'
 
  const Resume = () => {
     return (
-        <div className="container">
+        <div className="about">
             <h2 className='resumeTitle'>My Resume</h2>
+            <h2 className='resumeTitle1'>Click  here to view my resume</h2>
             <a href="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/015/987/original/Zhansaya_Sarmanova_Resume_Updated_.jpg?1629941548"><img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/015/982/original/651-6517838_icon-resume-icons-for-personal-resume-sketch-freebie.png?1629941207" alt="Resume" className="icon1" target="_blank" /></a>
-        
-        </div>
+                </div>
     )
 }
 export default Resume;
