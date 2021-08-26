a55b851f10b4bea3c0a890240fd7431168d687b3
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2021-08-25 19:17:54 -0500

    polished project page

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -2,9 +2,9 @@ import React from "react";
 const styles = {
   card:{
     float: 'right',
-    marginTop: "50px",
-    marginRight:'170px',
-    fontSize:'32px'
+    marginTop: "60px",
+    marginRight:'250px',
+    fontSize:'35px'
    
   }
 }
@@ -14,7 +14,7 @@ const styles = {
     <section className="about">
          <h1 className="title">About Me</h1>
          <img  id="avatar" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/011/043/original/IMG-0788.JPG?1624118358" alt="Zhansaya Sarmanova" />
-         <p className="parag" style={styles.card}>Hello, My name is Zhansaya Sarmanova. <br />I am full-stack Web Developer living in Austin,TX. <br /> I am very passionate about <br/>producing high level resultsand dedicated to my work. </p>
+         <p className="parag" style={styles.card}>Hello, My name is Zhansaya Sarmanova. <br />I am Full-Stack Web Developer living in Austin,TX. <br /> I am very passionate about <br />producing high level resultsand dedicated to my work.<br />Check out my Projects on next page. <br /> Also you can stop and say Hello at <br />zhansayasarmanova1993@gmail.com </p>
     </section>
 </div>
 
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -2,16 +2,32 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div className="card" style={{width: "18rem"}}>
-            <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/015/956/original/LandingPageEx.png?1629907022" className="card-img-top" alt="Pickflix & Grill"/>
-            <div className="card-body">
-                <br/>
-                <h5 className="card-title">Pickflix/Grill</h5>
-                <p className="card-text">Group 3 has created Pickflix & Grill, a movie and recipe generator that takes input from the user in the form of a movie genre and a recipe ingredient, and makes home date night decisions easier in a couple of clicks by accessing two server-side API's.</p>
-                <a href="https://cnvives.github.io/group-3/" className="btn btn-primary">View wedpage</a>
+        <section>
+            <div classname='images'>
+            <div className="row">
+                    <div className="column">
+                        <h1 className="imageTitle"><strong>Name:</strong> Sous Chef</h1>
+                        <a href="https://sous-chef-project-2.herokuapp.com/" target="_blank">
+                            <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/015/969/original/screenshot-2.png?1629931160" alt="Sous Chef" width="700" /></a>  </div>
+                    <div className="column">
+                        <h1 className="imageTitle"> <strong>Name:</strong> Pickflix & Grill</h1>
+                        <a href="https://cnvives.github.io/group-3/" target="_blank">
+                    <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/015/956/original/LandingPageEx.png?1629907022" alt="Pickflix & Grill" width="700" /></a>
+              </div>
+                    <div className="column">
+                        <h1 className="imageTitle"> <strong>Name:</strong>  Fitness Tracker</h1>
+                        <a href=" https://workout-tracker1993.herokuapp.com/?id=611b083c4939140016086c83" target="_blank">
+                    <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/015/970/original/1212.PNG?1629931237" alt="Fitness tracker" width="700" />
+                        </a>  </div>
+                </div>
+                <div className="column">
+                    <h1 className="imageTitle"> Weather Tracker</h1>
+                    <a href="  https://workout-tracker1993.herokuapp.com/?id=611b083c4939140016086c83" target="_blank">
+                        <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/015/971/original/Capture.PNG22.PNG?1629935146" alt="Weather tracker" width="700" /></a>
+                </div>
             </div>
-        </div>
-        
+            
+            </section>
     )
 }
 export default Projects;
